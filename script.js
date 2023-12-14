8c98f8a512cd279edf3182653bb22060e2340b66
Maciek <maciek.jamroz@post.pl>
2023-12-14 15:43:37 +0100

    Create function getComputerChoice

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+getComputerChoice () {
+
+}
+// A function that will randomly return 'Rock', 'Paper' or 'Sissors'.
+
+playRound (playerSelection, computerSelection) {
+
+}
+// A function that will return a string that declares the winner.
+// An example of return: "You Lose! Paper beats Rock".
+// Make playerSelection parameter case-insensitive.
+// Account for ties by repeating the round.
+
+game () {
+
+}
+// A function will use playRound within. 
+// Will play a best-of-fve game.
+// The funtion keeps the score
+// reports a winner or loser.
+*/
+
+
+
+
+
+function getComputerChoice() {
+    let random;
+    random = Math.round(Math.random() * 2 + 1);
+
+    if (random === 1) {
+        return "rock";
+    }
+    else if (random === 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
